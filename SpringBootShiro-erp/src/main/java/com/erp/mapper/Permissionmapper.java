9dc515b7f3fc39e260f034fbafce2e8effc95700
yaowenbin <yaowenbin@DESKTOP-M9GRPND>
2020-05-02 15:21:27 +0800

    55555

--- SpringBootShiro-erp/src/main/java/com/erp/mapper/Permissionmapper.java
+++ SpringBootShiro-erp/src/main/java/com/erp/mapper/Permissionmapper.java
@@ -3,7 +3,7 @@ package com.erp.mapper;
 import java.util.List;
 
 import com.erp.pojo.Permission;
-
+//4444444444444444444444
 public interface Permissionmapper {
 	
 	public List<Permission> selectPermission(String username);
